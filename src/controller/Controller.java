336158d21162dfb6a57a2d72f97d0b287ce12e1e
Nemanja Markovic <necamark@gmail.com>
2024-01-12 02:46:19 +0100

    Kreiranje dela modela i LoginForme

---
+++ src/controller/Controller.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Inzenjer;
+
+/**
+ *
+ * @author necam
+ */
+public class Controller {
+    private static Controller instance;
+    private List<Inzenjer> inzenjeri = new ArrayList<>();
+    private Inzenjer ulogovaniInzenjer = null;
+
+    public static Controller getInstance() {
+        if(instance == null) {
+            instance = new Controller();
+        }
+        return instance;
+    }
+
+    
+    
+    private Controller() {
+        Inzenjer i1 = new Inzenjer(1, "Nemanja", "Markovic", "neca123", "neca123");
+        Inzenjer i2 = new Inzenjer(2, "Petar", "Petrovic", "pera123", "pera123");
+        Inzenjer i3 = new Inzenjer(3, "Ana", "Avakumovic", "ana123", "ana123");
+        
+        inzenjeri.add(i1);
+        inzenjeri.add(i2);
+        inzenjeri.add(i3);
+    }
+
+    public List<Inzenjer> getInzenjeri() {
+        return inzenjeri;
+    }
+
+    public void setInzenjeri(List<Inzenjer> inzenjeri) {
+        this.inzenjeri = inzenjeri;
+    }
+
+    public Inzenjer getUlogovaniInzenjer() {
+        return ulogovaniInzenjer;
+    }
+
+    public void setUlogovaniInzenjer(Inzenjer ulogovaniInzenjer) {
+        this.ulogovaniInzenjer = ulogovaniInzenjer;
+    }
+
+    
+    
+    public Inzenjer login(String username, String lozinka) {
+        for (Inzenjer inzenjer : inzenjeri) {
+            if(inzenjer.getKorisnickoIme().equals(username) && inzenjer.getLozinka().equals(lozinka)) {
+                ulogovaniInzenjer = inzenjer;
+                return inzenjer;
+            }
+        }
+        return null;
+    }
+    
+    
+    
+}
---
+++ src/model/Inzenjer.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author necam
+ */
+public class Inzenjer {
+    private int id;
+    private String ime;
+    private String prezime;
+    private String korisnickoIme;
+    private String lozinka;
+
+    public Inzenjer() {
+    }
+
+    public Inzenjer(int id, String ime, String prezime, String korisnickoIme, String lozinka) {
+        this.id = id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    @Override
+    public String toString() {
+        return ime + " " + prezime;
+    }
+    
+    
+}
